da727885fb2b81a0abca00e8817c654c10c6e788
Huba Hanna <gubanna.11@gmail.com>
2022-10-09 23:01:59 +0300

    Group => services

--- agSalon/Controllers/GroupsController.cs
+++ agSalon/Controllers/GroupsController.cs
@@ -3,11 +3,13 @@ using agSalon.Data.Enums;
 using agSalon.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace agSalon.Controllers
@@ -21,11 +23,22 @@ namespace agSalon.Controllers
             _context = context;
         }
 
+
+        public async Task<IEnumerable<T>> GetAllAsync<T>(params Expression<Func<T, object>>[] includeProperties) where T:class
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+            return await query.ToListAsync();
+        }
+
         public async Task<IActionResult> Index()
         {
+            //var allGroups = await GetAllAsync<GroupsOfServices>(n => n.Services_Groups);
             var allGroups = await _context.Groups.ToListAsync();
 
-            //ViewBag.Services = _context.Groups.Where(g => g.Services_Groups.GroupId)
+            ViewBag.Services = await GetAllAsync<Service_Group>(n => n.Service);
 
             return View(allGroups);
         }
---
+++ agSalon/Data/Base/EtityBaserepository.cs
@@ -0,0 +1,30 @@
+﻿using agSalon.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace agSalon.Data.Base
+{
+    public class EtityBaseRepository<T> where T:class
+    {
+        private readonly AppDbContext _context;
+
+        public EtityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+            return await query.ToListAsync();
+        }
+
+    }
+}
