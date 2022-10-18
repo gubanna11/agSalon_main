46605985b713387515133d88c7b397b26eb193b3
Huba Hanna <gubanna.11@gmail.com>
2022-10-18 17:47:57 +0300

    New client

---
+++ agSalon/Controllers/ClientsController.cs
@@ -0,0 +1,44 @@
+﻿using agSalon.Data;
+using agSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace agSalon.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ClientsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var clients = _context.Clients.ToList();
+
+            return View(clients);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Client client)
+        {
+            if (!ModelState.IsValid)
+                return View(client);
+
+            await _context.Clients.AddAsync(client);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
